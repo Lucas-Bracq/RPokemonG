32bdbad59cae83129a6ee824b78debae3f0583a2
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2019-12-16 14:29:31 +0100

    reprise code combat fonctionnel
    léger changement reprenant le code de thomas pour effectuer des tests

---
+++ Assets/Code/BattleHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleHUD : MonoBehaviour
+{
+    public Text nameText;
+    public Slider hpSlider;
+
+    public void setHUD(Pokemon pokemon)
+    {
+        nameText.text = pokemon.name;
+
+        hpSlider.maxValue = pokemon.baseHp;
+        hpSlider.value = pokemon.currentHp;
+    }
+
+    public void setHp(int hp)
+    {
+        hpSlider.value = hp;
+    }
+}
